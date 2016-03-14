a66940a23be374f061a9016a8d2fd8c794ec0567
Markus Lohr <markus.lohr.acn@gmail.com>
2016-03-14 09:53:26 +0100

    add epyDoc strings

--- udp around the world/aws_ddb-writer.py
+++ udp around the world/aws_ddb-writer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+__author__ = "Markus Lohr"
+__copyright__ = "Copyright 2016, Markus Lohr"
+__credits__ = ["Markus Lohr"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Markus Lohr"
+__email__ = "markus.lohr.acn@gmail.com"
+__status__ = "Development"
+__note__ = """this code will out round trip timestamps in AWSdynamoDB"""
+                
 import threading
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
